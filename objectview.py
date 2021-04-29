d709adb81d156659339921c3d98509f54272b1ac
Jeff Fields <jkfields@yahoo.com>
2021-04-29 09:47:00 -0400

    Update objectview.py

--- objectview.py
+++ objectview.py
@@ -8,5 +8,6 @@ class ObjectView(dict):
     '''
     Enables a simple dictionary to be addressed as key-value pairs.
     '''
+     __delattr__ = dict.__delitem__
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
