72f6fe70b4ef6a6fb4bc6c2b3dfd8729ca13088f
Jeff Fields <jkfields@yahoo.com>
2021-04-29 09:31:44 -0400

    Create objectview.py

---
+++ objectview.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+'''
+Saw this simplified version in a post somewhere and tried it; it worked as well in my
+case as the longer version by @gpollatos (https://www.github.com/gpollatos/objectview
+'''
+class ObjectView(dict):
+    '''
+    Enables a simple dictionary to be addressed as key-value pairs.
+    '''
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
