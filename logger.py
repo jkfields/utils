bc9f3885d7207fe7c4efcdcf819ade65a124c0f6
Jeff Fields <jkfields@yahoo.com>
2024-10-06 15:19:46 -0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -7,15 +7,19 @@ import os
 DEFAULT_FORMAT = "%(asctime)s %(levelname)s [%(module)s %(funcName)s:%(lineno)d] - %(messages)s"
 DEFAULT_DATEFMT = "%Y-%m-%dT%H:%M:%S"
 
-'''
-Configure logging; if a config file (logging.ini) is not found in the current directory, 
-a default basic configuration is used
-'''
-try:
-    fileConfig('logging.ini')
-finally:
-    logging.basicConfig(format=DEFAULT_FORMAT,
-                        level=logging.DEBUG,
-                        datefmt=DEFAULT_DATEFMT)
-
-return logging.getLogger(__name__)
+def setup_logging(
+    name: str = __name__, 
+    format: str = DEFAULT_FORMAT,
+    level: int = logging.DEBUG,
+    dtformat : str = DEFAULT_DATEFMT
+) -> logging.Logger:
+    """
+    Configure logging; if a config file (logging.ini) is not found in 
+    the current directory, a default basic configuration is used
+    """
+    try:
+        fileConfig('logging.ini')
+    except:
+        logging.basicConfig(format=format, level=level, datefmt=dtformat)
+    finally:
+        return logging.getLogger(__name__)
